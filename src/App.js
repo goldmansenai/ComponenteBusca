eab2b54ed758f64474e142b82124f9e743ac5ca0
Vinícius Sanches Geniselli <vsgeniselli@gmail.com>
2021-11-19 17:04:15 -0300

    Projeto de busca filtrada de pokemons concluído

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import Pokebola from "./Pokebola.svg";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [pokemons, setPokemons] = useState([]);
+  const [busca, setBusca] = useState("");
+
+  const pokemonFilter = pokemons.filter((name) => {
+    return Object.values(name)
+      .join("")
+      .toLowerCase()
+      .includes(busca.toLowerCase());
+  });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(
+        "https://pokeapi.co/api/v2/pokemon?limit=901"
+      );
+      const data = await response.json();
+      setPokemons(data.results);
+    };
+
+    fetchData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="form-group mx-sm-3 mb-2 form-inline">
+        <label className="sr-only">Digite o nome de um Pokemon:</label>
+        <input
+          type="text"
+          onChange={(e) => {
+            setBusca(e.target.value);
+          }}
+          value={busca}
+          className="form-control"
+        />
+      </div>
+      <div className="d-flex">
+        <ul className="list-group mx-auto justify-content-center">
+          {pokemonFilter.map(({ name }) => (
+            <li className="list-group-item list-group-item-warning">
+              <div className="position">
+                <img src={Pokebola} alt="Pokebola"/>
+                <span className="px-2">
+                  {Object.values(name).join("").toUpperCase()}
+                </span>
+                <img src={Pokebola} alt="Pokebola"/>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
